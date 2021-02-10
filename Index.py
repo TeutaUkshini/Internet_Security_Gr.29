2b40aca610da1d2fe1f9e2bca443ce2a95fc443b
Tahir Temaj <tahir1temaj@gmail.com>
2021-02-10 22:27:15 +0100

    Update Index.py
    Vazhdimi i krijimit te titullit ,kornizes dhe ikonave te dritares

--- Index.py
+++ Index.py
@@ -37,3 +37,43 @@ def Scrape(arg=None):
             # Krijimi i ikones(logo)
             root.iconbitmap('img/logo.ico')
 
+# Bllokimi i zgjerimit te madhesise se dritares(Window)
+            root.resizable(width=False, height=False)
+            
+            ''' Perfundimi i dritares(Window)'''
+            
+            # Shtimi i scrollbar ne dritare(window)
+            scrollbar = Scrollbar(root)
+            scrollbar.pack(side=RIGHT, fill=Y)
+
+            # Perdorimi i widget text per te shfaqur permbajtjen scraped
+            text = Text(root, yscrollcommand=scrollbar.set, wrap = WORD)
+            text.insert(INSERT, info)
+            text.pack()
+
+            # Scroll bar
+            scrollbar.config(command=text.yview)
+
+        except ValueError:
+            ms.showerror('Gabim', 'Shenoni nje URL valide !!!')
+    
+'''Startimi i dritares(Window)'''
+# Krijimi Widget
+crawler = tk.Tk()
+
+# Krijimi i madhesise se dritares(Window)
+crawler.geometry('500x500')
+
+# Bllokimi i madhesise se dritares(Window)
+crawler.resizable(width=False, height=False)
+
+# Krijimi i titullit
+crawler.title('URL and structure crawling ')
+
+# Krijimi i ikones(logo)
+crawler.iconbitmap('img/logo.ico')
+''' Perfundimi i dritares(Window) '''
+
+# Korniza(Frame) Kryesore
+top_frame = Label(crawler, text='WEB CRAWLER',font = ('Cosmic', 25, 'bold'), bg='cyan', fg='black', relief='groove',padx=500, pady=30, bd='5')
+top_frame.pack(side='top')
